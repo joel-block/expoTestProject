29b3da4774f8efba48359a50558b6787d2c042a7
Joel <joel.t.block@gmail.com>
2022-12-08 22:09:34 -0600

    Add exceptions in getSprite for multi-form Pokemon

--- screens/HomeScreen.service.js
+++ screens/HomeScreen.service.js
@@ -7,6 +7,40 @@ export async function getSprite(name = "bulbasaur", shiny = false, setter) {
         ? await fetch(
             "https://pokeapi.co/api/v2/pokemon-form/alcremie-vanilla-cream/"
           )
+        : name === "unown"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/unown-a/")
+        : name === "burmy"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/burmy-plant/")
+        : name === "cherrim"
+        ? await fetch(
+            "https://pokeapi.co/api/v2/pokemon-form/cherrim-sunshine/"
+          )
+        : name === "shellos"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/shellos-east/")
+        : name === "gastrodon"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/gastrodon-east/")
+        : name === "arceus"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/arceus-normal/")
+        : name === "deerling"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/deerling-spring/")
+        : name === "sawsbuck"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/sawsbuck-spring/")
+        : name === "vivillon"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/vivillon-meadow/")
+        : name === "flabebe"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/flabebe-red/")
+        : name === "floette"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/floette-red/")
+        : name === "florges"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/florges-red/")
+        : name === "xerneas"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/xerneas-active/")
+        : name === "silvally"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/silvally-normal/")
+        : name === "sinistea"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/sinistea-phony/")
+        : name === "polteageist"
+        ? await fetch("https://pokeapi.co/api/v2/pokemon-form/polteageist-phony/")
         : await fetch(`https://pokeapi.co/api/v2/pokemon-form/${name}/`);
     const pokemonForm = await response.json();
     const imageURL = shiny
